9e58b8c1904439162db6ff5d789a462fff134092
Max Tolkachev <zhrivodkin@gmail.com>
2017-06-21 01:09:20 +0300

    markup / desktop / footer

--- src/components/O_Footer/index.js
+++ src/components/O_Footer/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router'
 // import * as T from "prop-types";
-import catnip from 'catnip';
 import './styles.scss';
-const cn = catnip('O_Footer');
+import {cssClassName} from 'utils'
+import A_Image from "../A_Image/index";
+const cn = cssClassName('O_Footer')
 const links = [
   { section: 'product', links: [{ name: 'Features', url: '#' }, { name: 'Open source', url: '#' }, { name: 'Humaniq wiki', url: '#' }] },
   { section: 'company', links: [{ name: 'Blog', url: '#' }, { name: 'News', url: '#' }, { name: 'Use cases', url: '#' }, { name: 'Partners', url: '#' }, { name: 'Events', url: '#' }]},
   { section: 'legal', links: [{ name: 'Privacy', url: '#' }, { name: 'Security', url: '#' }, { name: 'Policies', url: '#' }]},
   { section: 'resources', links: [{ name: 'Support', url: '#' }, { name: 'Contact us', url: '#' }, { name: 'Download mobile app', url: '#' }]}
+]
+const socials = [
+  { network: 'facebook', link: '#'},
+  { network: 'twitter', link: '#'},
+  { network: 'github', link: '#'},
+  { network: 'youtube', link: '#'},
+  { network: 'slack', link: '#'}
 ]
 
 function linksList() {
@@ -26,15 +34,25 @@ function linksList() {
   ))
 }
 
+function socialsList() {
+  return socials.map( ({network, link}) => (
+    <Link to={link} className={cn('soc-item')} title={network} key={network}>
+      <img src={"/img/social/" + network + ".svg"}/>
+    </Link>
+  ))
+}
+
 const O_Footer = () => {
   return (
     <footer className={cn()}>
       <div className="l-container">
-        <nav className={cn('nav')}>{linksList()}</nav>
-        <div className={cn('aux')}>
-          <div className={cn('lang')}>English (UK)</div>
-          <div className={cn('soc')}>
-
+        <div className={cn('inner')}>
+          <nav className={cn('nav')}>{linksList()}</nav>
+          <div className={cn('aux')}>
+            <div className={cn('lang')}>🇬🇧 English (UK)</div>
+            <div className={cn('soc')}>
+              {socialsList()}
+            </div>
           </div>
         </div>
       </div>
