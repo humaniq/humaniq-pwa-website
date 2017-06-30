9eda401f3f7430df10b2b461b043efee14b80ac2
kherel <kherel@users.noreply.github.com>
2017-06-30 21:40:05 +0800

    [footer] / add accordion

--- src/components/O_Footer/index.js
+++ src/components/O_Footer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router'
 // import * as T from "prop-types";
 import './styles.scss';
@@ -6,6 +6,7 @@ import {cssClassName} from 'utils'
 const cn = cssClassName('O_Footer')
 import {Motion, spring} from 'react-motion';
 import M_Select from 'M_Select';
+import A_Link from 'A_Link'
 
 const links = [
   {
@@ -30,71 +31,96 @@ const socials = [
   {network: 'slack', link: '#'}
 ]
 
-function linksList() {
-  return links.map(({section, links}) => (
-    <div className={cn('nav-section')} key={'key=' + section}>
-      <div className={cn('nav-section__title')}>{section}</div>
-      <ul className={cn('nav-list')}>
-        { links.map(({name, url}) => (
-          <li className={cn('nav-list__item')} key={'key=' + name}>
-            <Link to={url} className={cn('link')}>{name}</Link>
-          </li>
-        )) }
-      </ul>
-    </div>
-  ))
-}
+class O_Footer extends Component {
 
-function socialsList() {
-  return socials.map(({network, link}) => (
-    <Link to={link} className={cn('soc-item')} title={network} key={network}>
-      <img src={"/img/social/" + network + ".svg"}/>
-    </Link>
-  ))
-}
+  state = {
+    openedSection: null
+  }
 
-const O_Footer = ({menuOpen}) => {
-  const max = 100;
-  return (
-    <footer className={cn()}>
-      <Motion
-        defaultStyle={{x: 0}}
-        style={{
-          x: spring(menuOpen ? max : 0),
-          y: menuOpen ? spring(max) : 0
-        }}
-      >
-        {({x, y}) =>
-          <div className="l-container" style={{
-            transform: `translate3d(0, -${x}px, 0)`,
-            opacity: `${y === 0 ? 1 : y / max}`,
-            marginTop: `${x === 0 ? 0 : max}px`
-          }}>
-            <div className={cn('inner')}>
-              <nav className={cn('nav')}>{linksList()}</nav>
-              <div className={cn('aux')}>
-                <M_Select
-                  options={[
-                    {value: 'en', label: <span><span style={{top: 1, marginRight: 13, position:'relative'}}>🇬🇧</span>English (UK)</span>},
-                    {value: 'ch', label: <span><span style={{top: 1, marginRight: 13, position:'relative'}}>🇨🇳</span>中文</span>},
-                    {value: 'es', label: <span><span style={{top: 1, marginRight: 13, position:'relative'}}>🇪🇸</span>Español</span>},
-                    {value: 'de', label: <span><span style={{top: 1, marginRight: 13, position:'relative'}}>🇩🇪</span>Deutsch</span>}
-                  ]}
-                  value = {'en'}
-                  onChange={val => alert(val)}
-                />
-                <div className={cn('soc')}>
-                  {socialsList()}
+  handleTongleSection = (section) => {
+    const {openedSection: oldSection} = this.state
+    if(oldSection === section){
+      this.setState({openedSection: null})
+    }else{
+      this.setState({openedSection: section})
+    }
+  }
+
+  linksList(openedSection) {
+    return links.map(({section, links}) => {
+      const isOpen = openedSection === section
+      return (
+        <div className={cn('nav-section')} key={'key=' + section}>
+          <div
+            className={cn('nav-section__title', {isOpen})}
+            onClick={() => this.handleTongleSection(section)}
+          >{section}</div>
+          <ul className={cn('nav-list', {isOpen})}>
+            { links.map(({name, url}) => (
+              <li className={cn('nav-list__item')} key={'key=' + name}>
+                <A_Link to={url} disabled={url === '#'} >{name}</A_Link>
+              </li>
+            )) }
+          </ul>
+        </div>
+      )
+    })
+  }
+
+  socialsList() {
+    return socials.map(({network, link}) => (
+      <Link to={link} className={cn('soc-item')} title={network} key={network}>
+        <img src={"/img/social/" + network + ".svg"}/>
+      </Link>
+    ))
+  }
+
+  render(){
+    const {menuOpen} = this.props
+    const {openedSection} = this.state
+    const max = 100;
+    const renderedSocialList = this.socialsList()
+    const renderedLinksList = this.linksList(openedSection)
+    return (
+      <footer className={cn()}>
+        <Motion
+          defaultStyle={{x: 0}}
+          style={{
+            x: spring(menuOpen ? max : 0),
+            y: menuOpen ? spring(max) : 0
+          }}
+        >
+          {({x, y}) =>
+            <div className="l-container" style={{
+              transform: `translate3d(0, -${x}px, 0)`,
+              opacity: `${y === 0 ? 1 : y / max}`,
+              marginTop: `${x === 0 ? 0 : max}px`
+            }}>
+              <div className={cn('inner')}>
+                <nav className={cn('nav')}>{renderedLinksList}</nav>
+                <div className={cn('aux')}>
+                  <M_Select
+                    options={[
+                      {value: 'en', label: <span><span style={{top: 1, marginRight: 13, position:'relative'}}>🇬🇧</span>English (UK)</span>},
+                      {value: 'ch', label: <span><span style={{top: 1, marginRight: 13, position:'relative'}}>🇨🇳</span>中文</span>},
+                      {value: 'es', label: <span><span style={{top: 1, marginRight: 13, position:'relative'}}>🇪🇸</span>Español</span>},
+                      {value: 'de', label: <span><span style={{top: 1, marginRight: 13, position:'relative'}}>🇩🇪</span>Deutsch</span>}
+                    ]}
+                    value = {'en'}
+                    onChange={val => alert(val)}
+                  />
+                  <div className={cn('soc')}>
+                    {renderedSocialList}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        }
-      </Motion>
-    </footer>
-  )
+          }
+        </Motion>
+      </footer>
+    )
+  }
 }
-
 
 O_Footer.propTypes = {};
 
