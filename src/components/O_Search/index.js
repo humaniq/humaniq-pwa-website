0d519afb2fc0cad47904606c5dcf95704535580c
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-07-19 16:55:35 +0300

    fix bugs after feedback

--- src/components/O_Search/index.js
+++ src/components/O_Search/index.js
@@ -16,20 +16,20 @@ class O_Search extends Component {
       <div>
         <A_Container type='wide'>
           <A_H type='section wiki-title'>Welcome to Humaniq wiki</A_H>
-          <form action="" className={`search ${ active ? 'search__input--focused': '' }`}>
+          <form action="" className={cn(`search ${ active ? 'o-search__input--focused': '' }`)}>
             <input
               type="text"
-              className="search__input"
+              className={cn('input')}
               placeholder="Search any transaction"
               onFocus={() => this.setState({ active: true })}
               onBlur={() => this.setState({ active: false })}
             />
-            <button className="search__submit">
-              <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19 19">
+            <button className={cn('submit')}>
+              <svg className={cn('search-icon')} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19 19">
                 <g fill="none" transform="translate(-1 -1)">
                   <path fill="none" d="M0 0h20v20H0z"/>
-                  <path className="search-icon__path" d="M13.54 12.2l-1.429 1.4 4.906 4.907a.995.995 0 0 0 1.418.003.997.997 0 0 0 0-1.414L13.539 12.2z"/>
-                  <circle className="search-icon__circle" cx="8" cy="8" r="6.5"/>
+                  <path className={cn('icon-path')} d="M13.54 12.2l-1.429 1.4 4.906 4.907a.995.995 0 0 0 1.418.003.997.997 0 0 0 0-1.414L13.539 12.2z"/>
+                  <circle className={cn('icon-circle')} cx="8" cy="8" r="6.5"/>
                 </g>
               </svg>
             </button>
