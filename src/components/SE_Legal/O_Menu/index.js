4e4f570be893739e1ea3c0b42c2fb8e2621a3b19
nick <nicolai.voronin@gmail.com>
2017-07-19 11:10:11 +0300

    Create Menu component

---
+++ src/components/SE_Legal/O_Menu/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import * as T from "prop-types";
+import './styles.scss';
+import {cssClassName} from 'utils'
+const cn = cssClassName('O_Menu');
+import A_Link from 'A_Link';
+import A_P from 'A_P';
+
+
+const O_Menu = ({ options, activeLink, handleNavigation }) => {
+  const renderedLinks = options.map(({ anchor, text }) => {
+    const isActive = activeLink === anchor;
+    return (
+      <li key={'menu-item-' + anchor} className={cn('nav-links-item', { active: isActive })}>
+        {
+          isActive
+            ? <A_P type="hero">{text}</A_P>
+            : <A_Link to={anchor} type="primary" name={anchor} onClick={handleNavigation}>{text}</A_Link>
+        }
+      </li>
+    )
+  });
+
+  return (
+    <nav>
+      <ul className={cn()}>
+        { renderedLinks }
+      </ul>
+    </nav>
+  )
+};
+
+
+O_Menu.propTypes = {
+  options: T.arrayOf(T.shape({
+    anchor: T.string,
+    text: T.string.isRequired,
+  })),
+  activeLink: T.bool,
+  handleNavigation: T.func,
+};
+
+O_Menu.defaultProps = {
+  options: [],
+  activeLink: false,
+};
+
+export default O_Menu
