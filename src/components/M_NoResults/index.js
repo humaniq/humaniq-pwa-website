b1133e52bbcbec8102349cd113ec1e32c560c6aa
kherel <kherel@users.noreply.github.com>
2017-07-23 18:41:08 +0800

    fix no_result

--- src/components/M_NoResults/index.js
+++ src/components/M_NoResults/index.js
@@ -4,15 +4,18 @@ import './styles.scss';
 import {cssClassName} from 'utils'
 const cn = cssClassName('M_NoResults')
 
-const M_NoResults = ({text}) =>(
-  <div className={cn()}>Sorry! We looked everywhere, but couldn't find “<span className={'request'}>{text}</span>”. 💔 🔍</div>
+const M_NoResults = ({request}) =>(
+  <div className={cn('root')}>
+    <p className={cn('first')}>Sorry! We looked everywhere, but couldn't find “<span className={cn('request')}>{request}</span>”. 💔 🔍</p>
+    <p className={cn('second')}>Try a different search, or browse our help topics instead.</p>
+  </div>
 )
 
 M_NoResults.propTypes = {
-  text: T.string.isRequired
+  request: T.string.isRequired
 };
 
 M_NoResults.defaultProps = {
-  text: ''
+  request: ''
 }
 export default M_NoResults
