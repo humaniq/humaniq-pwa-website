f2ac721aa6f94b14efb5a5236bc5ce9bde8b6d3c
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-08-03 14:44:23 +0300

    fix styles in SE_OpenSource, SE_ContactUs, SE_Error

--- src/components/SE_OpenSource/index.js
+++ src/components/SE_OpenSource/index.js
@@ -11,17 +11,20 @@ const SE_OpenSource = () =>{
   
   const renderedLinks = libraries.map( ({name, description, url, type, imgSrc}) => {
     return(
-      <A_Link to={url} type="block" >
-        <div className={cn('container-item', {type})} key={'key_' + name}>
-          <div className={cn('container-item-image')}>
-            <img src={imgSrc} alt='Humaniq Android' />
+      <div className="card-item">
+        <A_Link to={url} type="block" >
+          <div className={cn('container-item', {type})} key={'key_' + name}>
+            <div className={cn('container-item-image')}>
+              <img src={imgSrc} alt='Humaniq Android' />
+            </div>
+            <div className={cn('container-text')}>
+              <A_H type='openitem'>{name}</A_H>
+              <p className={cn('card-item__description')}>{description}</p>
+            </div>
           </div>
-          <div className={cn('container-text')}>
-            <A_H type='openitem'>{name}</A_H>
-            <A_P type='fifth'>{description}</A_P>
-          </div>
-        </div>
-      </A_Link>
+        </A_Link>
+      </div>
+
     )
   })
 
@@ -30,21 +33,17 @@ const SE_OpenSource = () =>{
       <A_Container type='equal'>
         <div className={cn('opensource')}>
 
-          <A_Container type='section-clean'>
-            <div className={cn('header')}>
-              <div className={cn('header-image')}>
-                <img src="/img/opensource/open-source-image.svg" alt='Humaniq OpenSource'/>
-              </div>
-              <div className={cn('header-description')}>
-                <A_H type='hero'>Open Source</A_H>
-                <A_P type='hero'>Humaniq relies on open-source software and likes to give back to the community. Check out the libararies we have built.</A_P>
-              </div>
+          <div className={cn('header')}>
+            <div className={cn('header-image')}>
+              <img src="/img/opensource/open-source-image.svg" alt='Humaniq OpenSource'/>
             </div>
-          </A_Container>
+            <div className={cn('header-description')}>
+              <A_H type='hero'>Open Source</A_H>
+              <A_P type='hero'>Humaniq relies on open-source software and likes to give back to the community. Check out the libararies we have built.</A_P>
+            </div>
+          </div>
 
-          <A_Container type='section-clean'>
-            <A_H type='section'>Released Libraries</A_H>
-          </A_Container>
+          <A_H type='section'>Released Libraries</A_H>
 
           <A_Container type='section-clean'>
             <div className={cn('container')}>
@@ -52,15 +51,14 @@ const SE_OpenSource = () =>{
             </div>
           </A_Container>
 
-          <A_Container type='section-clean'>
-            <div className={cn('help')}>
-              <A_H type='section'>Help us Develop</A_H>
-              <A_P type='second'>Join Humaniq ever growing network. Get in touch if you would like to code with us.</A_P>
-              <A_P type='hero'>
-                <A_Link to='mailto:info@humaniq.com' target='_blank' type='primary'>Drop off your information here »</A_Link>
-              </A_P>
-            </div>
-          </A_Container>
+          <div className={cn('help')}>
+            <A_H type='section'>Help us Develop</A_H>
+            <span className={cn('subtitle')}>Join Humaniq ever growing network. Get in touch if you would like to code with us.</span>
+            <A_P type='hero'>
+              <A_Link to='mailto:info@humaniq.com' target='_blank' type='primary'>Drop off your information here »</A_Link>
+            </A_P>
+          </div>
+
         </div>
       </A_Container>
     </div>
