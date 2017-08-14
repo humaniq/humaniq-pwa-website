451cde5efb885d11da2d8e3cdcaf33b8fa443759
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-08-14 21:21:20 +0300

    timeline refactoring

--- src/components/SE_About/Timeline/index.js
+++ src/components/SE_About/Timeline/index.js
@@ -5,8 +5,52 @@ const cn = cssClassName('O_Timeline');
 import A_Container from 'A_Container';
 import A_H from 'A_H';
 
+const data = [
+  {
+    year: '2016',
+    month: {
+      name: 'April',
+      events: [
+        'Inception of the Humaniq idea by Alex Fork.',
+      ]
+    }
+  },
+]
 
 class O_Timeline extends Component {
+
+  renderEvents = (events) => {
+    const renderedEvents = events.map(event => (
+      <li className={cn('month-list__li')}>
+        <p className={cn('month-list__p')}>{event}</p>
+      </li>
+    ));
+
+    return (
+      <ul className={cn('month-list')}>
+        {renderedEvents}
+      </ul>
+    )
+  };
+
+  renderYear = (year) => {
+    return (
+      <section className={cn('year')}>
+        <h3 className={cn('year-date')}>{year.year}</h3>
+        { this.renderMonth(year.month) }
+      </section>
+    )
+  };
+
+  renderMonth = (month) => {
+    return (
+      <section className={cn('month')}>
+        <h4 className={cn('month-name')}>{month.name}</h4>
+        {this.renderEvents(month.events)}
+      </section>
+    )
+  };
+
   render() {
     return (
       <div>
@@ -14,107 +58,14 @@ class O_Timeline extends Component {
           <div className={cn('timeline')}>
             <A_H type='section-c'>A quick walk through our history as a company:</A_H>
             <div className={cn('border-line')}>
-              <section className={cn('year')}>
-                <h3 className={cn('year-date')}>2016</h3>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>April</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Inception of the Humaniq idea by Alex Fork.</p>
-                    </li>
-                  </ul>
-                </section>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>November</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Humaniq Whitebook is written and the original project team is formed.</p>
-                    </li>
-                  </ul>
-                </section>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>December</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Humaniq.co website is launched and pre-ICO is announced</p>
-                    </li>
-                  </ul>
-                </section>
-              </section>
-              <section className={cn('year')}>
-                <h3 className={cn('year-date')}>2017</h3>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>February</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Smart contracts are being developed, due diligence and marketing campaign take place.</p>
-                    </li>
-                  </ul>
-                </section>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>February</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Humaniq team meets with project partners in India. Alex Fork conducts the first performance tests of the project in unbanked regions of India.</p>
-                    </li>
-                  </ul>
-                </section>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>February</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Humaniq announces first online hackathon</p>
-                    </li>
-                  </ul>
-                </section>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>February</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Pre-ICO (crowdsale) is launched</p>
-                    </li>
-                  </ul>
-                </section>
-
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>March</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Alpha version of the app is available for testing on IOS and Android.</p>
-                    </li>
-                  </ul>
-                </section>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>April</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Humaniq collects $1.5 mln in the first hour of their ICO.</p>
-                    </li>
-                  </ul>
-                </section>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>May</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>App with new enchanced biometrics is being released.</p>
-                    </li>
-                  </ul>
-                </section>
-                <section className={cn('month')}>
-                  <h4 className={cn('month-name')}>Now</h4>
-                  <ul className={cn('month-list')}>
-                    <li className={cn('month-list__li')}>
-                      <p className={cn('month-list__p')}>Humaniq is 43 people now, working from 14 countries.</p>
-                    </li>
-                  </ul>
-                </section>
-              </section>
+              {this.renderYear(data[0])}
             </div>
           </div>
         </A_Container>
       </div>
     )
   }
+
 }
 
 
