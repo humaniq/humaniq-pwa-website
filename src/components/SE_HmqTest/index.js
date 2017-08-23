c7d95eaebd3fe41be5b416b8ef9670c7b2b58963
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-08-23 19:27:58 +0300

    [update] O_Transiton/Holder.js

--- src/components/SE_HmqTest/index.js
+++ src/components/SE_HmqTest/index.js
@@ -6,14 +6,13 @@ import O_Transaction from 'O_Transaction';
 
 
 class SE_HmqTest extends Component {
-
   render() {
     return (
       <div>
         <div className={cn('content')}>
-          <O_Transaction type="log" />
-          <O_Transaction type="log" />
-          <O_Transaction type="log" />
+          <O_Transaction type="log" {...{txHash: 'txHash', block: 'block', from: 'from', to: 'to', time: 'to', usdAmount: 'usdAmount', hmqAmount: 'hmqAmount'}}/>
+          <O_Transaction type="log" {...{txHash: 'txHash', block: 'block', from: 'from', to: 'to', time: 'to', usdAmount: 'usdAmount', hmqAmount: 'hmqAmount'}}/>
+          <O_Transaction type="log" {...{txHash: 'txHash', block: 'block', from: 'from', to: 'to', time: 'to', usdAmount: 'usdAmount', hmqAmount: 'hmqAmount'}}/>
         </div>
 
       </div>
