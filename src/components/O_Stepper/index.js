addf6474d5169daf625805c810a6002950ca11cf
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-08-23 10:29:17 +0300

    [update] O_stepper

--- src/components/O_Stepper/index.js
+++ src/components/O_Stepper/index.js
@@ -4,25 +4,34 @@ import {cssClassName} from 'utils';
 const cn = cssClassName('O_Stepper');
 
 class O_Stepper extends Component {
+  state = {
+    stepsCompleted: [1]
+  };
+
+  setStep = (step) => {
+    const { stepsCompleted } = this.state;
+    this.setState({ stepsCompleted: [...stepsCompleted, step] });
+  };
 
   render() {
+    const { stepsCompleted } = this.state;
     return (
       <div>
         <ul className={cn('list')}>
-          <li className={`${cn('step')} ${cn('step--done')}`}>
+          <li className={cn('step', { step: this.state.stepsCompleted.includes(1)})} onClick={() => this.setStep(1)}>
             <div className={cn('bar')}></div>
             <div className={cn('dot')}></div>
-            <div className={cn('txt')}>Lorem ipsum dolor.</div>
+            <div className={cn('txt')}>Шаг первый</div>
           </li>
-          <li className={`${cn('step')} ${cn('step--next')}`}>
+          <li className={cn('step', { step: this.state.stepsCompleted.includes(2)})} onClick={() => this.setStep(2)}>
             <div className={cn('bar')}></div>
             <div className={cn('dot')}></div>
-            <div className={cn('txt')}>Lorem ipsum dolor.</div>
+            <div className={cn('txt')}>Шаг второй</div>
           </li>
-          <li className={cn('step')}>
+          <li className={cn('step', { step: this.state.stepsCompleted.includes(3)})} onClick={() => this.setStep(3)}>
             <div className={cn('bar')}></div>
-            <div className={cn('dot')}></div>
-            <div className={cn('txt')}>Lorem ipsum dolor.</div>
+            <div className={cn('dot')} ></div>
+            <div className={cn('txt')}>Шаг третий</div>
           </li>
         </ul>
       </div>
