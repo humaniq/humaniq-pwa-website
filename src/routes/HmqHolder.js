7dd7f7664c687cb1cd83c0ef31ae28505e8dea99
kherel <kherel@users.noreply.github.com>
2017-08-29 20:51:56 +0800

    [fix]

--- src/routes/HmqHolder.js
+++ src/routes/HmqHolder.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {fetchHmqHolder, fetchHmqHolderT, cleanHmqHolderT} from 'store/entities/hmqHolder/actions'
+import {fetchHmqHolder, fetchHmqHolderT} from 'store/entities/hmqHolder/actions'
 import initialLoad from 'utils/initialLoad'
 import HmqHolderContainer from 'containers/HmqHolderContainer'
 
@@ -7,15 +7,8 @@ class HmqHolder extends Component {
 
   static prepareData({dispatch, getState}, query, params) {
     if(initialLoad() || !params.holder) return;
-    const state = getState()
-    dispatch(cleanHmqHolderT())
     dispatch(fetchHmqHolderT(params.holder))
 
-    // if(state.address !== params.holder){
-    //   dispatch(cleanHmqHolderT())
-    // } else {
-    //   dispatch(fetchHmqHolderT(params.holder))
-    // }
     return dispatch(fetchHmqHolder(params.holder))
   }
 
