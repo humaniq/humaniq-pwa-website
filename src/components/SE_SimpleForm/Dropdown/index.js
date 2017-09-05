b6d574c3636a079f4940f78ffeea949378633c13
German Topalov <gaijin773@yandex.ru>
2017-09-05 14:24:59 +0700

    [upd] fixed dropdown added

--- src/components/SE_SimpleForm/Dropdown/index.js
+++ src/components/SE_SimpleForm/Dropdown/index.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import * as T from "prop-types";
 import './styles.scss';
 import {cssClassName} from 'utils'
-const cn = cssClassName('M_Dropdown')
+const cn = cssClassName('M_Dropdown2')
 import onClickOutside from 'react-onclickoutside'
+import A_InputText from 'A_InputText'
 
-class M_Dropdown extends Component {
+class M_Dropdown2 extends Component {
   state = {
-    isOpened: false
+    isOpened: false,
+    value: this.props.value || '',
+    inputValue: ''
   }
 
   handleClickOutside = () => {
-    this.closeMenu()
+    this.setState({isOpened:false, inputValue: this.state.value})
   }
 
-  closeMenu(){
-    this.setState({isOpened:false})
-  }
 
   handleClick = () =>{
     if(!this.state.isOpened)
@@ -24,21 +24,30 @@ class M_Dropdown extends Component {
   }
 
   handleChange = (selected) => {
-    this.closeMenu()
+    this.setState({inputValue: selected, isOpened:false, value: selected})
     this.props.onChange(selected)
   }
 
   getListOptions(options, isOpened, selected) {
-    const renderOptions = options.map(option => {
-      return (
-        <ol
-          className={cn('list-options-item', {selected: selected === option})}
-          value={option}
-          key={'key_' + option}
-          onClick={() => this.handleChange(option)}
-        >{option}</ol>
-      )
-    })
+    let renderOptions
+
+    if(options.length){
+      renderOptions = options.map(option => {
+        return (
+          <ol
+            className={cn('list-options-item', {selected: selected === option})}
+            key={'key_' + option}
+            onClick={() => this.handleChange(option)}
+          >{option}</ol>
+        )
+      })
+    }else{
+      renderOptions = <ol
+        className={cn('list-options-item')}
+        onClick={() => this.setState({inputValue: ''})}
+
+      >No results</ol>
+    }
 
     return (
       <div className={cn('list-wrapper', {closed: !isOpened})}>
@@ -49,30 +58,52 @@ class M_Dropdown extends Component {
     )
   }
 
+
+  getOptions(options, filter){
+    return(
+      options.filter(country => country.toLowerCase().includes(filter.toLowerCase()))
+    )
+  }
+
   render() {
-    const {options, selected} = this.props
-    const {isOpened} = this.state
+    const {options, selected, error} = this.props;
+    const {isOpened, inputValue} = this.state;
+    const filteredOptions = this.getOptions(options, inputValue)
+    const listOptions = this.getListOptions(filteredOptions, isOpened, selected)
     return (
       <span className={cn('root')} onClick={this.handleClick}>
-        <div className={cn('select')}>
-          <div className={cn('select-text')}>{selected}</div>
+        <div className={cn('select', {isOpened})}>
+          <div className={cn('inner')}>
+            <A_InputText
+              setFocus = {isOpened}
+              label="Your country"
+              value = {inputValue}
+              onFocus = {() => this.setState({inputValue: ''})}
+              handleChange={value => this.setState({inputValue: value})}
+            />
+
+          </div>
+
         </div>
-        {this.getListOptions(options, isOpened, selected)}
+        {listOptions}
+        <div className={cn('error')}>{error}</div>
+
       </span>
 
     )
   }
 }
 
-M_Dropdown.propTypes = {
+M_Dropdown2.propTypes = {
   options: T.array.isRequired,
-  selected: T.string.isRequired
+  selected: T.string.isRequired,
+  error: T.string
 };
 
-M_Dropdown.defaultProps = {
+M_Dropdown2.defaultProps = {
 }
 
-export default onClickOutside(M_Dropdown)
+export default onClickOutside(M_Dropdown2)
 
 
 
