3f9f14598703c169dbccf032b194cd06aa4adce3
German Topalov <gaijin773@yandex.ru>
2017-10-27 19:13:34 +0700

    [update] forms partly done

--- src/components/h_widgets/A_Btn_H/index.js
+++ src/components/h_widgets/A_Btn_H/index.js
@@ -5,14 +5,32 @@ import {cssClassName} from 'utils'
 
 const cn = cssClassName('A_Btn_H');
 
-const A_Btn_H = ({mix, children}) => (
-  <div className={cn([mix])}>
-    {children}
-  </div>
-)
+const A_Btn_H = ({mix, children, type}) => {
+  switch(type) {
+    case 'submit':
+      return (
+        <button
+          className={cn([mix])}
+          type='submit'
+        >
+          {children}
+        </button>
+      )
+    default:
+      return (
+        <div className={cn([mix])}>
+          {children}
+        </div>
+      )
+  }
+
+}
 
 export default A_Btn_H
 
 A_Btn_H.propTypes = {
   mix: T.string, //BEM mixin from parent block
+  type: T.oneOf([
+    'submit', // form submit btn
+  ])
 }
