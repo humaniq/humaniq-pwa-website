af0b1a61e5326f99b6e56f5dd2a8efe45331b6d0
kherel <kherel@users.noreply.github.com>
2017-11-14 07:41:14 +0800

    [update] hide img

--- src/components/pages/Home/sections/OpportunitiesSection/index.js
+++ src/components/pages/Home/sections/OpportunitiesSection/index.js
@@ -3,52 +3,41 @@ import A_Title from 'A_Title_H'
 import SectionCounter from '../common/SectionCounter/index.js'
 import InfoColumns from '../common/InfoColumns'
 import './styles.scss'
-import {cssClassName} from 'utils'
+import { cssClassName } from 'utils'
 
-const cn = cssClassName('SE_Home_Opportunities');
+const cn = cssClassName('SE_Home_Opportunities')
 
-const SE_Home_Opportunities = ({mix}) => (
+const SE_Home_Opportunities = ({ mix }) => (
   <section className={cn([mix])}>
     <div className={cn('content')}>
-      <A_Title
-        mix={cn('title')}
-        type='section'
-        theme='dark'
-      >
+      <A_Title mix={cn('title')} type="section" theme="dark">
         Extraordinary opportunitites for business
       </A_Title>
 
-      <InfoColumns
-        mix={cn('info-columns')}
-        columns={infoColumns}
-        type='narrow'
-      />
-
+      <InfoColumns mix={cn('info-columns')} columns={infoColumns} type="narrow" />
     </div>
-    <SectionCounter
-      sectionNum={7}
-      theme='bright'
-    />
+    <SectionCounter sectionNum={7} theme="bright" />
   </section>
 )
 
 export default SE_Home_Opportunities
 
-
 const infoColumns = [
   {
     imageSrc: '/img/design-v2/icons/sections/etherium.svg',
     title: 'Based on Ethereum blockchain protocol',
-    text: 'Guaranteed security against hacking, corruption and loss of information thanks to complete decentralisation'
+    text:
+      'Guaranteed security against hacking, corruption and loss of information thanks to complete decentralisation',
   },
   {
     imageSrc: '/img/design-v2/icons/sections/accelerator.svg',
     title: 'Independent fund and accelerator',
-    text: 'External projects and promising ideas can receive expert support and investment for a launch into the Humaniq system'
+    text:
+      'External projects and promising ideas can receive expert support and investment for a launch into the Humaniq system',
   },
   {
     imageSrc: '/img/design-v2/icons/sections/api.svg',
     title: 'Open API',
-    text: 'Any project can merge into the Humaniq system'
-  }
+    text: 'Any project can merge into the Humaniq system',
+  },
 ]
