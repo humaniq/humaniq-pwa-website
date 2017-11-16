38644cf8ae7928e064e341d4d577a8ebd9e941a9
German Topalov <gaijin773@yandex.ru>
2017-11-16 18:35:33 +0700

    [update] components refactoring, styles refactoring begining

--- personalinitializers/server/globals.js
+++
@@ -1,3 +0,0 @@
-global.__SERVER__ = true
-global.__CLIENT__ = false
-global.__DEVELOPMENT__ = (process.env.NODE_ENV || '').toLowerCase() === 'development'
--- personalinitializers/server/noSSR.js
+++
@@ -1,9 +0,0 @@
-import webpackAssets from './webpackAssets';
-import Helmet from 'react-helmet'
-
-export default function(req, res){
-  res.status(200);
-  const helmet = Helmet.rewind()
-
-  res.render('index', {helmet, webpackAssets})
-}
