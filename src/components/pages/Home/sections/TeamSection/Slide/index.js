62c2b31e455d2d61696a044943d59e559407299c
Kherel Kechil <kherel@gmail.com>
2018-02-23 13:24:10 +0700

    [update]

--- src/components/pages/Home/sections/TeamSection/Slide/index.js
+++ src/components/pages/Home/sections/TeamSection/Slide/index.js
@@ -4,17 +4,24 @@ import './styles.scss'
 import { cssClassName } from 'utils'
 const cn = cssClassName('TeamSectionSlide')
 import A_Image from 'A_Image'
+import M_Tooltip from 'M_Tooltip'
 
 const getHazemName = () => <span>Hazem Danny<br />Al-Nakib</span>
 
 const TeamSectionSlide = ({persons, groupName, hidden}) => {
-  const getSlide = ({imgSrc, position, name}, i) => (
+  const getSlide = ({imgSrc, position, name, bio}, i) => (
     <div className={cn('person')} key={i}>
       <div className={cn('person-image')}>
         <A_Image src={imgSrc} rounded />
       </div>
       <p className={cn('person-name')}>{name.search('Al-Nakib') === -1 ? name : getHazemName()}</p>
       <p className={cn('person-title')}>{position}</p>
+
+      {bio &&
+        <span className={cn('tooltip')} >
+          <M_Tooltip type={'bottom'}>{bio}</M_Tooltip>
+        </span>
+      }
     </div>
   )
   const renderTitle = groupName && <span className={cn('group-name')}>{groupName}</span>
