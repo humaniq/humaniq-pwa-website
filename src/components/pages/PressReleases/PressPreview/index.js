64bfc1623a09e5cd672e6ad0325f15909e2a1dde
German Topalov <ru00161131@gmail.com>
2018-02-26 18:22:52 +0700

    [upd] press preview added

---
+++ src/components/pages/PressReleases/PressPreview/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as T from "prop-types";
+import './styles.scss';
+import {cssClassName} from 'utils'
+const cn = cssClassName('PressPreview')
+
+const PressPreview = ({mix, title, date, text}) => (
+  <div className={cn([mix])}>
+    <p className={cn('title')}>{title}</p>
+    <p className={cn('date')}>{date}</p>
+    <p className={cn('text')}>{text}</p>
+  </div>
+)
+
+
+
+PressPreview.propTypes = {
+  mix: T.string
+}
+
+export default PressPreview
--- src/components/pages/PressReleases/page.js
+++ src/components/pages/PressReleases/page.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './styles.scss';
 import {cssClassName} from 'utils'
 import SearchPanel from './SearchPanel'
+import PressPreview from './PressPreview'
 import A_Container from 'A_Container'
 import A_Title_H from 'A_Title_H'
 import A_Btn from 'A_Btn'
@@ -44,11 +45,59 @@ class PressReleases extends Component {
           </A_Btn>
         </div>
         {showAll && <SearchPanel />}
-
+        {!showAll && (
+          <div>
+            <A_Title_H
+              type='subtitle'
+              theme='blue'
+              mix={cn('subtitle')}
+            >
+              The most popular
+            </A_Title_H>
+            <div className={cn('previews-container')}>
+              {news.map(({title, date, text}) => (
+                <PressPreview
+                  mix={cn('preview')}
+                  title={title}
+                  date={date}
+                  text={text}
+                />
+              ))}
+            </div>
+          </div>
+        )}
 
       </A_Container>
     )
   }
 }
+
+const news = [
+  {
+    title: 'Huffingtonpost.com',
+    date: '05/16/2017',
+    text: 'Tackling Financial Exclusion With Biometric Technology'
+  },
+  {
+    title: 'Huffingtonpost.com',
+    date: '05/16/2017',
+    text: 'Tackling Financial Exclusion With Biometric Technology'
+  },
+  {
+    title: 'Huffingtonpost.com',
+    date: '05/16/2017',
+    text: 'Tackling Financial Exclusion With Biometric Technology'
+  },
+  {
+    title: 'Forbes.com:',
+    date: '04/21/2017',
+    text: 'From Here To Where? Bitcoin And The Future Of Cryptocurrency'
+  },
+  {
+    title: 'Forbes.com:',
+    date: '04/21/2017',
+    text: 'From Here To Where? Bitcoin And The Future Of Cryptocurrency'
+  },
+]
 
 export default PressReleases
