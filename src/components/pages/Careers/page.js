68c70c065f538ab5276c9f63a8a7b8ad4effbe10
Kherel Kechil <kherel@gmail.com>
2018-04-05 01:16:38 +0700

    [add] careers

--- src/components/pages/Careers/page.js
+++ src/components/pages/Careers/page.js
@@ -14,7 +14,24 @@ const Careers = ({ entities, loaded, loading }) => {
       <A_Title_H type="section" mix={cn('title')}>Careers</A_Title_H>
       {!loading && loaded && !entities.length ? (
         <p className={cn('no-vacancies')}>
-          There are currently no vacancies but <a href={`mailto:hr@humaniq.com`} target="_blank">please send</a> your resume for future consideration.
+          Humaniq is constantly growing and looking for valuable assets for our team. The list of Humaniq vacancies gets updated regularly, so if you share our values and wish to join us, we will be happy to welcome your CVs at <a href={`mailto:hr@humaniq.com`} target="_blank">hr@humaniq.com</a>.
+
+          We are currently interested in specialists with the focus on:
+          <br/>
+          <br/>
+
+          <ul>
+            <li>Product management</li>
+            <li>Customer development</li>
+            <li>Analytics and Targeting</li>
+            <li>SMM & Community</li>
+            <li>E-mail marketing</li>
+            <li>Blockchain Development</li>
+
+          </ul>
+          <br/>
+
+          Should you not have found a vacancy of interest on this list, we will in any case be happy to receive a resume from you. As soon as we have an opening that corresponds to your skills, we will be sure to get in touch.
         </p>
       ) : (
         <O_CareersList entities={entities} />
