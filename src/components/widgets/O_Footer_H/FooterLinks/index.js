d548733ec24e38712cdc9774f2ebf9d1d5a370dd
Viacheslav Kaloshin <kiltum@kiltum.tech>
2018-05-22 12:23:20 +0300

    Change link to app store

--- src/components/widgets/O_Footer_H/FooterLinks/index.js
+++ src/components/widgets/O_Footer_H/FooterLinks/index.js
@@ -112,7 +112,7 @@ const footerLinks = [
   {
     section: 'Humaniq app',
     links: [
-      {name: 'Easy-to-use mobile app and financial infrastructure for billions of unbanked hmq.im/app', url: 'https://hmq.im/app', externalLink: true},
+      {name: 'Easy-to-use mobile app and financial infrastructure for billions of unbanked hmq.im/app', url: 'https://hmq.onelink.me/JU9Z/5679f4a0', externalLink: true},
     ]
   }
 ]
