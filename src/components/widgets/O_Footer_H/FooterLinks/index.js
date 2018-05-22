bcf5d6d14cb62cd201c0501337d831b75ec5cc7e
Viacheslav Kaloshin <kiltum@kiltum.tech>
2018-05-22 15:16:13 +0300

    change footer text

--- src/components/widgets/O_Footer_H/FooterLinks/index.js
+++ src/components/widgets/O_Footer_H/FooterLinks/index.js
@@ -112,7 +112,7 @@ const footerLinks = [
   {
     section: 'Humaniq app',
     links: [
-      {name: 'Easy-to-use mobile app and financial infrastructure for billions of unbanked hmq.im/app', url: 'https://hmq.onelink.me/JU9Z/5679f4a0', externalLink: true},
+      {name: 'Download in Google Play', url: 'https://hmq.onelink.me/JU9Z/5679f4a0', externalLink: true},
     ]
   }
 ]
