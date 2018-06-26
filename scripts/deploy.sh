6844179341e50a2512aac4a29ffda9a956e1d70b
Viacheslav Kaloshin <kiltum@kiltum.tech>
2018-06-26 16:21:00 +0300

    quick fix for new deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-set -e
+set -x
 
 export IMAGE_NAME=pwa-website
 export SERVICE_NAME=$IMAGE_NAME
-export CHART_PATH=helm
+export CHART_PATH=deploy/helm
 export APP_VERSION=`git describe --tags --always HEAD | head -n 1`
 
 deploy() {
-    curl $GOOGLE_DEPLOY_HELPERS > /tmp/deploy_helpers.sh
-    source /tmp/deploy_helpers.sh google_auth
-    gcloud docker -- pull eu.gcr.io/humaniq-168420/site:base
-    source /tmp/deploy_helpers.sh deploy
+    source ./scripts/deploy_helper.sh google_auth
+    docker pull eu.gcr.io/humaniq-168420/site:base
+    source ./scripts/deploy_helper.sh deploy
 }
 
 deploy
