d84e8fd780e41a8844bc660ebe0e2f3957f613f4
Искандер Хакимов <wham@list.ru>
2018-07-19 15:34:51 +0300

    Added /news-1 page, routes etc

---
+++ src/components/pages/News1/sections/TeamSection/data.js
@@ -0,0 +1,271 @@
+const imgPath = 'img/team_s'
+const ambassadorsPath = 'img/ambassadors'
+
+export const advisers = [
+  {
+    name: 'David Applefield',
+    position: 'Adviser',
+    bio:
+      'Is an American writer and media specialist living in Paris. He has as spent his career developing creative and innovative solutions for business leaders, heads of state, ministries, NGOs, and other not-for- profit foundations. He is the Financial Times representative for Africa, the Middle East and emerging markets.',
+    imgSrc: `${imgPath}/david_applefield.jpg`,
+  },
+  {
+    name: 'Matt McKibbin',
+    position: 'Adviser',
+    bio:
+      'Is a business development guru and a decentralization evangelist. He became interested in Bitcoin and the blockchain technology in 2012. Technologically savvy, he is an extroverted connector driven by a desire to see transformative and decentralized technologies positively affect the 7 billion people on the planet. He is based in the Washington DC metro area.',
+    imgSrc: `${imgPath}/matt_mckibbin.jpg`,
+  },
+  {
+    name: 'Alex Bausch',
+    position: 'Adviser',
+    bio:
+      'Is an entrepreneur with broad experience in all aspects of international business development and the creation of global technology companies. He strongly believes in partner ecosystems which aim to inspire and spawn new innovations. His specialties include: SaaS services, mobile identity security, the IoT, enterprise managed mobility, decentralised ledger technology, marketing, and product development.',
+    imgSrc: `${imgPath}/alex_bausch.jpg`,
+  },
+  {
+    name: 'Nick Ayton',
+    position: 'Adviser',
+    bio:
+      'Is a writer, speaker and educator of all things Blockchain and Cryptocurrency. He is a Fintech 100 Influencer and Number 21 Rise Blockchain Top 100. He has had eight startups and held many senior roles in major IT and tech companies. He now chairs various Blockchain events and advises a range of clients on Blockchain technology, bitcoin and Ethereum. One of his latest projects is to raise funds using cryptoeconomics and create a new film finance model to make a mini- series for TV about the Bitcoin Revolution, an asset backed Token, and a new Media and Film Production business on Ethereum.',
+    imgSrc: `${imgPath}/nick_ayton.jpg`,
+  },
+  {
+    name: 'Michael Terpin',
+    position: 'Adviser',
+    bio:
+      "Michael Terpin co-founded BitAngels (www.bitangels.co), the world's first angel network for digital currency startups, in May 2013, and now serves as its chairman. The distributed angel network currently has more than 600 members, and it has made more than 25 investments in bitcoin and cryptocurrency companies with two exits and several follow-on rounds. Terpin is also co-founder and managing partner of bCommerce Labs (www.bcommercelabs.com), the first technology incubator fund focused on e-commerce companies for bitcoin and the blockchain, and a partner at Flight VC.",
+    imgSrc: `${imgPath}/michael_terpin.jpg`,
+  },
+  {
+    name: 'Dmitry Kaminskiy',
+    position: 'Adviser',
+    bio:
+      'An innovative entrepreneur and investor active in Biotech, Fintech, Blockchain and artificial intelligence, Dmitry is co-founder of the Deep Knowledge Ventures investment fund, focused on investments in AI and Deeptech. He adheres to the principle of impact investment in all his business activities, believing that business should make a social impact, and he supports the deployment of Blockchain for Good, which led to his decision to support Humaniq from its inception.',
+    imgSrc: `${imgPath}/dmitry_kaminskiy.jpg`,
+  },
+  {
+    name: 'Paolo Tasca',
+    position: 'Adviser',
+    bio:
+      'Paolo Tasca is a FinTech economist specialising in P2P Financial System. An advisor for different international organisations including the EU Parliament on blockchain technologies, Paolo recently joined the University College London as Director of the Centre for Blockchain Technologies (UCL CBT).  Prior to that, he has been a senior research economist at Deutsche Bundesbank working on digital currencies and P2P lending. Paolo is the co-author of the bestseller "FINTECH Book" and the co-editor of the book "Banking Beyond Banks and Money.',
+    imgSrc: `${imgPath}/paolo_tasca.jpg`,
+  },
+  {
+    name: 'Paul Mears',
+    position: 'Adviser',
+    bio:
+    'Paul started his career with Ernst and Young in London and is a UK qualified CPA. Over the last 30 years he has worked in a wide range of industries including Telecoms, Construction, Software, Hedge Funds in a wide range of CFO to Operational Finance roles.\n' +
+    'Mr. Mears left the UK in 1999 and has since lived and worked in New York, Vancouver, Amsterdam, Hong Kong and is currently a resident of Monaco. Paul has over the last 5 years been an active angel investor in early stage Tech, Apps, Medical Devices and with a passion for Stem Cell Biotech.\n' +
+    'Mr. Mears has a passion for ensuring creative artists are rewarded for their work and is a great believer in the ability of the blockchain to be a key player in that space and to ensure that artists are fairly rewarded for the work they create.',
+    imgSrc: `${imgPath}/paul_mears.jpg`,
+  }
+]
+
+export const team = [
+  {
+    name: 'Alex Fork',
+    position: 'Founder, CEO',
+    bio:
+      'Humaniq founder and Fintech entrepreneur Alex is one of the most important members of the international Blockchain community, who has worked with the UN to lead the deployment of Blockchain technology to improve the efficiency of UN resources and remittances. He has also spearheaded the harnessing of blockchains in Russia, organising a conference for 500 participants on the Ethereum Blockchain; authoring Bitcoin. More than Just Money; and founding the Fintech accelerator for startups in Russia, Future Fintech. He started Humaniq to help lift the global banked out of poverty.',
+    imgSrc: `${imgPath}/alex_fork.jpg`,
+  },
+  {
+    name: 'Javed Khattak',
+    position: 'CFO',
+    bio:
+      'Javed has a background as an actuary and of finance, consulting and startups. He loves technology, especially gadgets, AI and virtual reality, and believes in Blockchain for Good. He is working with Humaniq to bring greater opportunities and fairness to the world.',
+    imgSrc: `${imgPath}/javed_khattak.jpg`,
+  },
+  {
+    name: 'Hazem Danny Al-Nakib',
+    position: 'CIO',
+    bio: '',
+    imgSrc: `${imgPath}/hazem_danny.jpg`,
+  },
+  {
+    name: 'Anton Mozgovoy',
+    position: 'Chief Technical Officer',
+    bio:
+      'Anton was motivated to work with Humaniq to use his experience in both computer science and finance to help to bring happiness for all across the world. He is passionate about freedriving and wakeboarding, and, of course, staying late to finish work.',
+    imgSrc: `${imgPath}/anton_mozgovoy.jpg`,
+  },
+  {
+    name: 'Kirill Goryunov',
+    position: 'CPO',
+    bio: '',
+    imgSrc: `${imgPath}/kirill_goryunov.jpg`,
+  },
+  {
+    name: 'Kherel Kechil',
+    position: 'Lead Frontend Developer',
+    bio:
+      'Kherel is a digital nomad. He joined Humaniq to experience and participate in the financial evolution, and observe changes to the better in the country where he currently resides - China.',
+    imgSrc: `${imgPath}/kherel_kechil.jpg`,
+  },
+  {
+    name: 'Harry Bright',
+    position: 'CBDO',
+    bio:
+      'Bringing experience of management and economics, Serafima’s passions include art, music and Russian literature and the crypto-economy. She cannot stand inequality and is happy to have the opportunity to tackle it with Humaniq.',
+    imgSrc: `${imgPath}/harry_bright.jpg`,
+  },
+  {
+    name: 'Segrey Blinov',
+    position: 'SMM, CSS',
+    bio:
+      'Bringing experience of social media and customer service support, Segrey is passionate about Blockchain technology and his family.',
+    imgSrc: `${imgPath}/segrey_blinov.jpg`,
+  },
+  {
+    name: 'Danny Kass',
+    position: 'SMM, CSS',
+    bio:
+      'Danny’s background is in social media, B2B sales and customer service support. He loves cryptoeconomy and technology and at Humaniq is motivated to disrupt the financial industry with an ecosystem for entrepreneurs to make changes from the bottom up.',
+    imgSrc: `${imgPath}/danny_kass.jpg`,
+  },
+  {
+    name: 'Andrey Shevchenko',
+    position: 'SMM, CSS',
+    bio:
+      'Andriy has experience in computer science, security research, online community building and entrepreneurship and is passionate about both improving the world and space exploration. At Humaniq he wants to contribute to something good for humanity and boost financial independence.',
+    imgSrc: `${imgPath}/andriy_schechenko.jpg`,
+  },
+  {
+    name: 'Lee Baker',
+    position: 'Writer',
+    bio:
+      'Lee has a background in both communications and community organising and loves story-telling to inspire positive action and to connect with others to change things for the better. He wants to unleash the power of the collaborative economy and help everyone to put their destinies into their own hands.',
+    imgSrc: `${imgPath}/lee_baker.jpg`,
+  },
+  {
+    name: 'Olga Deribo',
+    position: 'Graphic Designer',
+    bio:
+      'Olga has a background in architecture, interior design and graphic design, and is passionate about inspiring others, building sites, spontaneity, and the universe. She is at Humaniq so she can be creative for social good - and to become a crypto geek!',
+    imgSrc: `${imgPath}/olga_derbio.jpg`,
+  },
+]
+
+export const ambassadors = [
+  {
+    name: "Achille Agbe Julien",
+    position: "Cote d’Ivoire",
+    "bio": "Achille graduated from the Abidjan University and EDHEC Business School with a final degree in Private Business Law (Master). Has gained an MBA and also runs his own consulting firm (Ismarten Ltd). ${ambassadorsPath}ille acts as the Managing Director of EIC Corporation.",
+    imgSrc: `https://s3-eu-west-1.amazonaws.com/humaniq-web-backend-public/ambassadors/pics/AchilleAgbeJulien.png`,
+  },
+  {
+    name: "Adewale Bankolo",
+    position: "Nigeria",
+    "bio": "Adewale Bankolo is a prominent Nigerian Libertarian. Adewale describes himself as \"very passionate about issues pertaining to entrepreneurship, free market, cryptocurrency, and financial inclusion\". He is the Chapter Director (Nigeria) at the Bastiat Society - an international society of principled wealth creators.",
+    imgSrc:  `${ambassadorsPath}/AdewaleBankolo.png`
+  },
+  {
+    name: "Alakanani Itireleng",
+    position: "Botswana",
+    "bio": "Alakanani is a qualified teacher. She has a BA in humanities, Post Graduate Diploma in Education and MBA in international business. Alakanani is an entrepreneur, she runs a blockchain technology hub Satoshicentre. Alakanani is a bitcoin enthusiastic and trainer.",
+    imgSrc:  `${ambassadorsPath}/AlakananiItireleng.png`
+  },
+  {
+    name: "Bashir Aminu",
+    position: "Nigeria",
+    "bio": "Founder of Cryptogene, the largest cryptocurrency community in Africa and a hub that aims at the application and use of blockchain tools by producing quality products and end-user services valuable to enthusiastic and engaged individuals.",
+    imgSrc:  `${ambassadorsPath}/BashirAminu.png`
+  },
+  {
+    name: "Chernoh Saeed Sow",
+    position: "Sierra Leone",
+    "bio": "CEO and founder of the Sierra Leone Bitcoin Network – SLBNet and also the Africoin Report blog on the Let’s Talk Bitcoin Network website. He is a tech entrepreneur and a bitcoin savvy person who has also been involved in many projects to help spread bitcoin knowledge in Sierra Leone and Africa as a whole.",
+    imgSrc:  `${ambassadorsPath}/ChernohSaeedSow.png`
+  },
+  {
+    name: "Donald Madzinga",
+    position: "South Africa",
+    "bio": "Donald was born in Zimbabwe but he currently lives in South Africa. He trades currencies as well as provides consultancy on Blockchain technology to corporates.",
+    imgSrc:  `${ambassadorsPath}/DonaldMadzinga.png`
+  },
+  {
+    name: "Itumeleng Isaac Lejone",
+    position: "Lesotho",
+    "bio": "Itumeleng is a founder and CEO of IIL Capital - an investment company that focuses on venture capitalism for upcoming entrepreneurs. They also partner with other technology companies to build IOT, blockchain, games and other technology products.",
+    imgSrc:  `${ambassadorsPath}/ItumelengIsaacLejone.png`
+  },
+  {
+    name: "Karabo Segale",
+    position: "South Africa",
+    "bio": "Karabo was born in South Africa and lives in North West Province in a small town called Rustenburg in Ramoga Village. Is an Ambassador for Humaniq, a businessman, is into trading and mining Bitcoins.",
+    imgSrc:  `${ambassadorsPath}/KaraboSegale.png`
+  },
+  {
+    name: "Kayode Babarinde",
+    position: "Nigeria",
+    "bio": "Kayode is currently working on \"Cryptocurrency and Blockchain Technology\" as his Masters project at the Centre for Entrepreneurship and Innovation, University of Ibadan, Nigeria.",
+    imgSrc:  `${ambassadorsPath}/KayodeBabarinde.png`
+  },
+  {
+    name: "Koketso Pelo",
+    position: "Botswana",
+    "bio": "Koketso is a director at Satoshi centre and is also studying software engineering in the Limkokwing University, with the area of interest in cryptocurrency and blockchain technology.",
+    imgSrc:  `${ambassadorsPath}/KoketsoPelo.png`
+  },
+  {
+    name: "Mogopodi Phiri",
+    position: "South Africa",
+    "bio": "Technician with both scientific and engineering backgrounds. In 2015 he was introduced to Bitcoin and studied crypto-currencies and Blockchain technology and its applications through the internet, networking and joining crypto-currency discussion groups on different social media platforms.",
+    imgSrc:  `${ambassadorsPath}/MogopodiPhiri.png`
+  },
+  {
+    name: "Mohammed Bukar",
+    position: "Nigeria",
+    "bio": "Mohammed is a Humaniq Ambassador representing Abuja, Nigeria. He has a degree in Computer science and Masters in Information systems. Mohammed is also a crypto currency and blockchain enthusiast.",
+    imgSrc:  `${ambassadorsPath}/MohammedBukar.png`
+  },
+  {
+    name: "Noah Roger Justin",
+    position: "Cameroon",
+    "bio": "Noah is a cryptocurrency investor and trader, running bitcoin educational courses.",
+    imgSrc:  `${ambassadorsPath}/NoahRogerJustin_2x.png`
+  },
+  {
+    name: "Oladimeji Shekoni",
+    position: "Nigeria",
+    "bio": "Oladimeji is a counselling Psychotherapist in Philadelphia Specialist Hospital, Lagos. Entrepreneur, Crypto-Enthusiast.",
+    imgSrc:  `${ambassadorsPath}/OladimejiShekoni.png`
+  },
+  {
+    name: "Peter Samson",
+    position: "Botswana",
+    "bio": "IT entrepreneur born in Mwanza region, currently living in Dar es Salaam region Tanzania. Peter has opened a Meetup group about Bitcoin just to bring bitcoiners together to discuss ways on how Bitcoin can be implemented in Tanzania. They prepared the Coinfest as a Bitcoin tradition to celebrate and exchange ideas about Bitcoin.",
+    imgSrc:  `${ambassadorsPath}/PeterSamson.png`
+  },
+  {
+    name: "Philip Agyei Asare",
+    position: "Ghana",
+    "bio": "Philip is the Founder and CEO of Pak Universal Services and the founder of Dream Foundation Ghana, a non-profit created to empower young entrepreneurs, whom he says are the intellectual, social and cultural lifeblood of a country. Philip is also a member of the Conscious Entrepreneurship Foundation.",
+    imgSrc:  `${ambassadorsPath}/PhilipAgyeiAsare.png`
+  },
+  {
+    name: "Roderick P. Clantina",
+    position: "Phillippines",
+    "bio": "Roderick is a private employee as an inventory assistant, but his area of interest is cryptocurrency and financial inclusion.",
+    imgSrc:  `${ambassadorsPath}/RoderickPClantina.png`,
+  },
+  {
+    name: "Pius Okedinachi",
+    position: "Bayelsa State Nigeria",
+    // "bio": "Pius Okedinachi bio",
+    imgSrc:  `${ambassadorsPath}/PiusOkedinachi.png`,
+  },
+  {
+    name: "Kabonire Ruhinda",
+    position: "Uganda",
+    // "bio": "Kabonire Ruhinda bio",
+    imgSrc:  `${ambassadorsPath}/KabonireRuhinda.png`,
+  },
+  {
+    name: "Md.Mofassair Hossain Dhaka",
+    position: "Bangladesh",
+    // "bio": "Md.Mofassair Hossain Dhaka bio",
+    imgSrc:  `${ambassadorsPath}/Md.MofassairHossainDhaka.png`,
+  }
+]
