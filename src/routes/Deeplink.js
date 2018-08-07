7aee1a8e5bfe82491917dc393ed35d97cf2ff8c1
mav <mavaleri4@gmail.com>
2018-08-07 16:33:07 +0600

    deeplinkin for app

---
+++ src/routes/Deeplink.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import DeeplinkContainer from 'components/pages/Deeplink/container'
+import initialLoad from 'utils/initialLoad'
+
+class DeeplinkRoute extends Component {
+
+  render() {
+    return (
+      <DeeplinkContainer />
+    )
+  }
+}
+
+export default DeeplinkRoute;
