4eb94817b5b5aac46a39a6ddfcceacb035493d14
mav <mavaleri4@gmail.com>
2018-09-18 14:31:01 +0600

    deeplink check

--- src/components/pages/Deeplink/page.js
+++ src/components/pages/Deeplink/page.js
@@ -6,35 +6,56 @@ const queryString = require('query-string');
 class Deeplink extends Component {
   componentDidMount() {
     var downloadURL = 'https://play.google.com/store/apps/details?id=com.humaniq.lite';
-    var redirectURL = 'chat/hmq_assistbot';
+    var redirectURL = '';
 
     if(typeof(location)!=='undefined'&&typeof(location.search)!=='undefined'&&location.search!==''){
       var parsedQuery = queryString.parse(location.search);
 
       if(typeof(parsedQuery)!=='undefined'&&parsedQuery!==null&&typeof(parsedQuery.l)!=='undefined'&&parsedQuery.l!==''){
-        var allowedPattern = /^(profile|request|send|chat|groupchat|bizchat)[A-z0-9/]*/g;
+        var allowedPatterns = [
+          /^profile\/(main|edit)$/g,
+          /^request$/g,
+          /^send\/[A-z0-9]+$/g,
+          /^chat\/[A-z0-9]+$/g,
+          /^groupchat\/[A-z0-9]+$/g,
+          /^bizchats$/g,
+          /^bizchat\/biz_[A-z0-9]+$/g,
+          /^stories$/g,
+          /^story\/[A-z0-9]+$/g,
+          /^support$/g,
+        ];
 
-	if(allowedPattern.test(parsedQuery.l)){
-          redirectURL = parsedQuery.l;
+        for(var i = 0; i < allowedPatterns.length; i++){
+          if(allowedPatterns[i].test(parsedQuery.l)){
+            redirectURL = parsedQuery.l;
+            break;
+          }
         }
       }
     }
 
-    redirectURL = 'humaniq://humaniq.com/' + redirectURL;
+    if(redirectURL!==''){
+      redirectURL = 'humaniq://humaniq.com/' + redirectURL;
+alert(redirectURL);
 
-    $('#button-dl').attr({'action':downloadURL}); 
-    $('#button-app').attr({'action':redirectURL}); 
+      $('#button-dl').attr({'action':downloadURL});
+      $('#button-app').attr({'action':redirectURL});
+      /*window.location = redirectURL;
 
-    window.location = redirectURL;
-    
-    setTimeout(()=>{
-      window.location = downloadURL;
-    }, 1000);
+      setTimeout(()=>{
+        window.location = downloadURL;
+      }, 1000);*/
+    }else{
+      $('#button-dl').hide();
+      $('#button-app').hide();
+      $('#errorDiv').show();
+    }
   }
 
   render() {
     return (
       <div style={{"maxWidth":"768px", "margin":"0 auto", "padding":"15px"}}>
+        <div id="errorDiv" style={{"color":"#f00", "textAlign":"center", "display":"none"}}>Wrong request!</div>
         <form id="button-dl" action="" method="GET" target="_blank">
             <A_Btn type="material-r-link-window" btnType="submit">Download</A_Btn>
         </form>
